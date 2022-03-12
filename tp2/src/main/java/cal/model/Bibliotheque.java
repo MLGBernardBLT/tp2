36e439d53bb94537ba73c4ac1b965051c4498238
Your Name <you@example.com>
2022-03-12 02:42:10 -0500

    Ajouts de constructeur et de getter/setter dans la classe Bibliotheque

--- tp2/src/main/java/cal/model/Bibliotheque.java
+++ tp2/src/main/java/cal/model/Bibliotheque.java
@@ -7,4 +7,20 @@ import java.util.List;
 public class Bibliotheque {
     private String nom;
     private List<Document> documents = new ArrayList<>();
+
+    public Bibliotheque(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public List<Document> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<Document> documents) {
+        this.documents = documents;
+    }
 }
