317b902e5ec31f5defedc0a3684e8c5ed7789f92
Your Name <you@example.com>
2022-03-12 02:43:41 -0500

    Ajouts du constructeur et de getter sur la classe Document

--- tp2/src/main/java/cal/model/Document.java
+++ tp2/src/main/java/cal/model/Document.java
@@ -9,4 +9,27 @@ public class Document {
     private String editeur;
     // TODO: 2022-03-12 Quel classe utiliser pour la date ?
     private Date anneePublication;
+
+    public Document(String titre, String auteur, String editeur, Date anneePublication) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.editeur = editeur;
+        this.anneePublication = anneePublication;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public String getEditeur() {
+        return editeur;
+    }
+
+    public Date getAnneePublication() {
+        return anneePublication;
+    }
 }
