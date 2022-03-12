4655ef51c9dcf2ff0dc439129bc75f7504fd755f
Your Name <you@example.com>
2022-03-12 02:35:25 -0500

    Creation de la classe Bibliotheque et debut de la classe Document

---
+++ tp2/src/main/java/cal/model/Bibliotheque.java
@@ -0,0 +1,8 @@
+package cal.model;
+
+import java.util.ArrayList;
+
+public class Bibliotheque {
+    private String nom;
+    private List<Document> documents = new ArrayList()<>;
+}
---
+++ tp2/src/main/java/cal/model/Document.java
@@ -0,0 +1,12 @@
+package cal.model;
+
+import java.sql.Date;
+
+
+public class Document {
+    private String titre;
+    private String auteur;
+    private String editeur;
+    // TODO: 2022-03-12 Quel classe utiliser pour la date ?
+    private Date anneePublication;
+}
